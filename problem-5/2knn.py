6896c7e65a75d5ea907c622d877ea913ac99b490
Fernando Soares <fernando@mekros>
2018-04-06 17:15:16 -0300

    Problem 5 done

--- problem-5/2knn.py
+++ problem-5/2knn.py
@@ -1,6 +1,7 @@
+import sys
 import math
 import random
-#import matplotlib.pyplot as plt
+from collections import Counter
 
 # Retorna a media das respostas das k entradas mais proximas ao novo_exemplo
 # 1) Calcula a distancia euclidiada (sqrt(sum((x - y)^2))) entre novo_exemplo e cada uma das entradas
@@ -10,28 +11,23 @@ import random
 # 5) Calcula a media e retorna o valor
 
 def knn(novo_exemplo, entradas, respostas, k):
-    melhores =  sorted(
-                        [
-                         [
-                             math.sqrt(
-                                 sum(
-                                  [(n - e)**2 for n, e in zip(novo_exemplo, entradas[j])]   # Calculo da distancia euclidiana entre dois vetores
-                                 )
-                             ), respostas[j], "id="+str(j)
-                         ] for j in range(len(entradas))
-                        ],
-                        key=lambda tupla: tupla[0]
-                )[:k]
+    distancias = []
+    for i in range(len(entradas)):
+        distancias.append(
+            math.sqrt(sum([(n-e)**2 for n, e in zip(novo_exemplo, entradas[i])]))
+        )
+    tupla = zip(distancias, respostas)
+    ordem = sorted(tupla, key= lambda k: k[0])[:k]
 
-    valores = [m[1] for m in melhores]
-    # return float(sum(valores))/len(valores)
+    valores = [m[1] for m in ordem]
+    
     ###### IMPLEMENTE AQUI O RETORNO DA RESPOSTA MAIS FREQUENTE DENTRE OS K VIZINHOS MAIS PROXIMOS
-
+    return Counter(valores).most_common()[0][0]
 
 ########################
 # Main
 ########################
-         
+
 # Le a linha com as configuracoes
 config = sys.stdin.readline().strip().split(';')
 
@@ -50,36 +46,17 @@ respTreino = [random.choice(cores[:nDim]) for _ in range(tamTreino)]
 respTeste =  [random.choice(cores[:nDim]) for _ in range(tamTeste)]
 
 vetK = [1, 3, 5]
+erros = []
 for valK in vetK:
-    erros = []
+    contador = []
     estimado = []
-    
     for i in range(tamTeste):
         estimado.append(knn(dadosTeste[i], dadosTreino, respTreino, valK))
-        #erros.append(abs(estimado[i] - respTeste[i]))
+        contador.append(estimado[i] == respTeste[i])
 
-    #erroMedio = sum(erros)/float(len(erros))
     ###### IMPLEMENTE AQUI O CALCULO DA ACURACIA
-    
+    erros.append(float(contador.count(True))/float(tamTeste))
 
 # Saida
 for valor in erros:
     print ("%.3f" % valor)
-
-
-# Desenha os conjuntos de dados
-#plt.figure()
-#
-#for i in range(tamTreino):
-#    plt.plot( dadosTreino[i][0], dadosTreino[i][1], 'x', markersize=10, color = respTreino[i], mew=3)
-#    plt.text(dadosTreino[i][0], dadosTreino[i][1], str(i), fontsize=14, color="whitesmoke")
-#    plt.text(dadosTreino[i][0]-0.005, dadosTreino[i][1]+0.005, str(i), fontsize=14)
-#
-#for i in range(tamTeste):
-#    plt.plot( dadosTeste[i][0], dadosTeste[i][1], 'o', markersize=10, color = respTeste[i])
-#    plt.text(dadosTeste[i][0], dadosTeste[i][1], str(i), fontsize=14, color="whitesmoke")
-#    plt.text(dadosTeste[i][0]-0.005, dadosTeste[i][1]+0.005, str(i), fontsize=14)
-#
-#plt.draw()
-#plt.show()
-
